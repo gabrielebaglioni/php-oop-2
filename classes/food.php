f00440378c670758752653ff8fade63f5213f211
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-28 17:09:24 +0200

    add all constructor

--- classes/food.php
+++ classes/food.php
@@ -2,7 +2,12 @@
   require_once __DIR__ . '/Product.php';
 
   class Food extends Product{
-   public string $type;
-   public string $expire_date;
+   public  $type;
+   public  $expire_date;
+   public function __construct( string $_type, $_expire_date )
+   {
+    $this->type = $_type;
+    $this->expire_date = $_expire_date ;
+   }
   }
 ?>
